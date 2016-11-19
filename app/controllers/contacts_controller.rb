473d399e1a774a03d94b7d3d719613840c6a39b6
wjstienstra <wjstienstra@gmail.com>
2016-11-19 13:54:15 +0000

    Added mailer and contactform

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,6 +4,37 @@ class ContactsController < ApplicationController
     end
     
     def create
+        @contact = Contact.new(contact_params)
+        if @contact.save
+            name = params[:contact][:name]
+            email = params[:contact][:email]
+            body = params[:contact][:comments]
+            
+            ContactMailer.contact_email(name, email, body).deliver
+
+            flash[:succes] = "Message sent."
+            redirect_to new_contact_path
+        else
+            flash[:danger] = "Error occured, message has not been sent"
+            redirect_to new_contact_path
+        end
     end
+    
+      private
+        def contact_params
+          params.require(:contact).permit(:name, :email, :comments)
+        end
 end
 
+
+
+
+
+
+
+
+
+
+
+   
+ 
