8d255161e2a30c8454be9f7998878999fd3d9b3c
wjstienstra <wjstienstra@gmail.com>
2016-11-24 20:59:53 +0000

    added routes and controller for profiles

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
+  
+  recources :users do
+    resource :profile
+  end
+  
   resources :contacts
   get '/about' => 'pages#about'
   root 'pages#home'
